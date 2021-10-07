4935adc8f7049a81f9b0ed4aa7e70a195201b747
DevPermataIndonesia <it.dev@permataindonesia.com>
2021-10-07 22:47:25 +0700

    fixed

--- src/components/Head/index.tsx
+++ src/components/Head/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 
 export default function Header({ page }: any) {
     return (
-        <Head children >
+        <Head >
             <title>MOC | {page}</title>
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             <link rel="shortcut icon" href="/static/big-logo.png" />
